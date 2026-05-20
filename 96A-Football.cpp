671360e8b04ab6755095ca8d1819c08a5531e45e
Sakil-Anuar <anuarsakil7@gmail.com>
2026-05-20 17:21:44 +0600

    Solved today's CF problem 96A

---
+++ 96A-Football.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int count=1;
+
+    for(int i=1;i<s.size();i++){
+        if(s[i]==s[i-1]){
+            count++;
+        }
+        else{
+            count=1;
+        }
+        if(count>=7){
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+    return 0;
+
+}
